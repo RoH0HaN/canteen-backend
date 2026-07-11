b8889b8fcbf3d5c1afe371bd3757b7da9415b25c
RoH0HaN <rohan251@outlook.com>
2026-07-11 13:17:26 +0530

    feat: add partitioned attribute for cookies in login and refresh token methods

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -167,6 +167,7 @@ export const loginUser = asyncHandler(async (req, res, next) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
+    partitioned: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
@@ -225,6 +226,7 @@ export const refreshToken = asyncHandler(async (req, res, next) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
+    partitioned: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
